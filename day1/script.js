f0fd76f8afda9ece2c7e5fd6925134b3db429f6d
mrFazliddin23 <122162768+mrFazliddin23@users.noreply.github.com>
2024-02-19 07:43:14 +0500

    Variables, Alert, Confirm, Prompt

---
+++ day1/script.js
@@ -0,0 +1,17 @@
+//To'rburchakning yuzini va peremetrni hisoblaydigan dastur tuzing.////
+
+// const height = 1560
+// const width = 895
+// const heightMetr = height / 100;
+// const widthMetr = width / 100;
+// const area = `To'rtburchakning yuzi ${ Math.round(heightMetr * widthMetr)} ga teng`;
+// console.log(area);
+// const peremetr = `To'rturchakning peremetri ${Math.round(2*(heightMetr + widthMetr))} ga teng`;
+// console.log(peremetr);
+
+
+////task2 Do'koningiz bor. Har bir molning ustiga foiz qo'yib sotasiz. Pechenyeni qanchadir pulga oldingiz. Nechpul foyda ko'rasiz?///
+const apple = 5000;
+const applePercent = 10;
+const totalPrice = (5000 * 10) / 100
+console.log(`Total price = ${totalPrice}`);
