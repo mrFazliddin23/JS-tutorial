6a28dbc8d24faf6634a557a8b4577701c7e8d64b
mrFazliddin23 <122162768+mrFazliddin23@users.noreply.github.com>
2024-11-13 18:16:13 +0500

    if else, switch case

--- day2/script.js
+++ day2/script.js
@@ -1,4 +1,4 @@
-/*Task1: Foydalanuvchidan yoshni so’raydigan dasturni tuzing. Yosh 50 dan yuqori bo’lsa, dastur “Siz kattasiz” deb chiqarib berishi kerak. Yosh 50 dan past bo’lsa “Siz yoshsiz” deb chiqarib berishi kerak*/
+//Task1
 const age = +prompt("How old are you?", '')
 if (age >= 50){
     console.log("Siz qariyabsz")
@@ -7,3 +7,40 @@ else{
     console.log("Siz yoshsiz");
     
 }
+/* Task2:*/
+const price = +prompt("Somsa narxi qancha?")
+if (price > 3000){
+    console.log("Qimmat");
+}
+else{
+    console.log("Arzon");
+}
+//Task3:
+const password = prompt("Enter Password")
+switch(password){
+    case 'sarimsoqpiyoz':
+        alert("Hello");
+        break;
+    default:
+        alert("Parol xato");
+}
+//Task4
+const salary = Number(prompt("Oyligingiz soliqlardan tashqari qancha?"))
+let perYear = 0
+for (let i = 1; i <= 12; i++){
+    perYear += salary 
+}
+console.log(`Sizning yillik topadigan pulingiz ${perYear} mln`);
+//Task5:
+// const salary = Number(prompt("Oyligingiz soliqlardan tashqari qancha?"))
+// let xarajatlar = (salary * 90)/100;
+// let birOydaYigilganPul = salary - xarajatlar
+// let birYildaToplanganPul = birOydaYigilganPul * 12
+// console.log("Siz bir yilda " + birYildaToplanganPul + " mln pul yig'asz");
+
+
+
+
+
+
+
