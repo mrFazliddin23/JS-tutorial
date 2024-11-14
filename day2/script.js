90e83348fbda1aa7ef3057129ac27637f26b3829
mrFazliddin23 <122162768+mrFazliddin23@users.noreply.github.com>
2024-11-14 22:36:47 +0500

    cs

--- day2/script.js
+++ day2/script.js
@@ -1,7 +1,123 @@
+//Task1
+// const age = +prompt("How old are you?", '')
+// if (age >= 50){
+//     console.log("Siz qariyabsz")
+// }
+// else{
+//     console.log("Siz yoshsiz");
+// }
+/* Task2:*/
+// const price = +prompt("Somsa narxi qancha?")
+// if (price > 3000){
+//     console.log("Qimmat");
+// }
+// else{
+//     console.log("Arzon");
+// }
+//Task3:
+// const password = prompt("Enter Password")
+// switch(password){
+//     case 'sarimsoqpiyoz':
+//         alert("Hello");
+//         break;
+//     default:
+//         alert("Parol xato");
+// }
+//Task4
+// const salary = Number(prompt("Oyligingiz soliqlardan tashqari qancha?"))
+// let perYear = 0
+// for (let i = 1; i <= 12; i++){
+//     perYear += salary 
+// }
+// console.log(`Sizning yillik topadigan pulingiz ${perYear} mln`);
+//Task5:
+// const salary = Number(prompt("Oyligingiz soliqlardan tashqari qancha?"))
+// let xarajatlar = (salary * 90)/100;
+// let birOydaYigilganPul = salary - xarajatlar
+// let birYildaToplanganPul = birOydaYigilganPul * 12
+// console.log("Siz bir yilda " + birYildaToplanganPul + " mln pul yig'asz");
+
+// ------------------------------------//
+//task: 6
+// for (let i = 1; i <= 100; i++){
+//     if(i % 3 == 0){
+//         console.log("FIZZ");        
+//     }
+//     else if (i % 5 == 0) {
+//         console.log("BUZZ");        
+//     }
+//     else if (i % 3 == 0 && i % 5 == 0){
+//         console.log("FIZZBUZZ");
+//     }
+//     else{
+//         console.log(i);   
+//     }
+    
+// }
+
+// TAsk7: uch xonali Armstrong raqamlarni topish
+// const armstrongNums = [];
+// const notArmstrong = [];
+// for(let i = 100; i <= 999; i++){
+//     lastNum = i % 10
+//     middleNum = Math.floor((i % 100) / 10)
+//     firstNum = Math.floor(i / 100)
+//     lastNumKub = (lastNum*lastNum*lastNum )
+//     middleNumKub = (middleNum*middleNum*middleNum)
+//     firstNumkub = (firstNum*firstNum*firstNum)
+//     sum = lastNumKub + middleNumKub + firstNumkub
+//     if(i == sum){
+//     armstrongNums.push(i);
+//     }
+//     else{
+//         notArmstrong.push(i);
+//     }
+// }
+// console.log(armstrongNums);
+// console.log(notArmstrong);
+
+/* Task8: 
+*  
+* *  
+* * *  
+* * * *  
+* * * * * */
+// let i, j, char = "";
+// for(let i = 2; i < 10; i++){
+//        for(let j = 1; j < i; j++){
+//         char = char + ("*  ");
+//     }    
+//     console.log(char);
+//     char = "";
+// }
+
+//Task9
+// let arr1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; 
+// let arr2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26];
+// let sumArr1 = 0;
+// let result;
+// for(let i = 0; i < arr1.length; i++){
+//     sumArr1 += arr1[i] 
+// }
+// let sumArr2 = 0;
+// let j = 0;
+// while (j < arr2.length){
+//     sumArr2 += arr2[j]
+//     j++;
+// }
+// console.log(result = sumArr1 + sumArr2);
+//Task10:
+// let n = 30;
+// for (let i = 1; i <= n; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+//Task11:
 let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
 for(let i = 0; i < arr.length-1; i--){
 }
-console.log(arr[i]);
+// console.log(arr[i]);
 
 // let arr1 = [43, "what", 9, true, "cannot", false, "be", 3, true, aaa, bbb];
 // reverse = arr1.reverse()
